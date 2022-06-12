00d9c9b943822cde5008fa9924e1bf2208a06bd3
YousefiNafiseh <yousefi.nafiseh@gmail.com>
2022-06-12 23:59:35 +0430

    feature JavaScript part3 for receive notifications even if they registered themselves after such notification had been broadcast

---
+++ JavaScript/part3.js
@@ -0,0 +1,59 @@
+var Component = function () {
+	const events = {};
+	const publishedEvents = [];
+	let subscribersId = -1;
+
+	function callSubscriber(subscriber, data) {
+		subscriber.func(data);
+	}
+
+	this.publish = function (event, data) {
+		if (!events[event]) {
+			return false;
+		}
+
+		const subscribers = events[event];
+		subscribers.forEach((subscriber) => {
+			callSubscriber(subscriber, data);
+		});
+		publishedEvents.push({
+			event,
+			data
+		});
+		return true;
+	}
+
+	this.subscribe = function (event, func) {
+		if (!events[event]) {
+			events[event] = [];
+		}
+
+		subscribersId += 1;
+		const token = subscribersId.toString();
+		const subscriber = {
+			token,
+			func
+		};
+		events[event].push(subscriber);
+		publishedEvents.filter((item) => item.event === event).
+			forEach((item, index) => {
+				setTimeout(() => {
+					callSubscriber(subscriber, item.data)
+				}, (index + 1) * 2000);
+			});
+		return token;
+	}
+
+	this.unsubscribe = function (token) {
+		const found = Object.keys(events).some((event) => events[event].some((subscriber, index) => {
+			const areEqual = subscriber.token === token.toString();
+			if (areEqual) {
+				events[event].splice(index, 1);
+			}
+			return areEqual;
+		}));
+
+		return found ? token : null;
+	}
+
+}
