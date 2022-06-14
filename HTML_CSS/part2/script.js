2b2b351e0b65548add7d53c6bce957ddfbf8f0bb
YousefiNafiseh <yousefi.nafiseh@gmail.com>
2022-06-14 13:26:05 +0430

    refactor css part2 for removing important from css

--- HTML_CSS/part2/script.js
+++ HTML_CSS/part2/script.js
@@ -3,12 +3,12 @@ function trToggle(row) {
 	var progressContinerElement = row.getElementsByClassName('progress-bar-continer')[0];
 	if (row.classList.length === 0) {
 		row.classList.add('tr-toggle');
-		progressBarElement.classList.add('progress-bar-value-toggle');
+		progressBarElement.style.backgroundColor = "#cd0a1c";
 		progressContinerElement.classList.add('progress-bar-continer-toggle');
 	}
 	else {
 		row.classList.remove('tr-toggle');
-		progressBarElement.classList.remove('progress-bar-value-toggle');
+		progressBarElement.style.backgroundColor = null;
 		progressContinerElement.classList.remove('progress-bar-continer-toggle');
 	}
 }
